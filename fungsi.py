665db2d2e94ed632f3fdd7c054304913fc98e1cd
iammaverick13 <iam.marverick13@gmail.com>
2020-05-08 17:14:39 +0700

    menambahkan file fungsi.py yang berfungsi sebagai kalkulator sederhana

---
+++ fungsi.py
@@ -0,0 +1,16 @@
+def additional(a, b):
+	return a + b
+
+def subtraction(a, b):
+	return a - b
+
+def multiplication(a, b):
+	return a * b
+
+def divisor(a, b):
+	return a / b
+
+
+a = int(input('First number : '))
+b = int(input('Second number : '))
+print(additional(a, b))
